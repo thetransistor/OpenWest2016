cbce2ce100480cbd9dcd59d5316f84b0ea86a2a1
Animcogn <corbanvilla@gmail.com>
2016-07-15 16:43:02 -0600

    snake gets faster as game goes on

--- Firmware/ow10000-demo/ow10000-nibble.cpp
+++ Firmware/ow10000-demo/ow10000-nibble.cpp
@@ -27,13 +27,13 @@ void OW10000_nibble::reset() {
 	nibbleStack[1] = snake_location;
 	nibbleStack[2] = snake_location;
 	nibbleStack_length = 3; // size of the nibbleStack. Locations the snake is occupying
-	snakeSpeed = 250; // milliseconds between moves, lower is faster
+	snakeSpeed = 50; // milliseconds between moves, lower is faster
 	direction = 's';  // s->stop, u->up, d->down, l->left, r->right
 	score = 0;        // No game, no score. Sounds harse. Isn't.
 	stillPlaying = true; // Set to false to end game...
 	lastUpdate = millis(); // Game timer
 	badge -> clear();
-	
+
 	badge->setPixel((snake_location >> 4),(snake_location & 0b00001111),3);
 	getNextApple();
 }
@@ -42,16 +42,16 @@ void OW10000_nibble::reset() {
 // Get the next apple location
 void OW10000_nibble::getNextApple(){
 	bool foundSpot = false;
-	
+
 	while(!foundSpot){
 		// Generate a random spot
 		apple_location = (byte) random(256);
-		
+
 		if(!snakeCollision(apple_location)){
 			foundSpot = true;
 		}
 	}
-	badge->setPixel((apple_location >> 4),(apple_location & 0b00001111),2);	
+	badge->setPixel((apple_location >> 4),(apple_location & 0b00001111),2);
 }
 
 
@@ -78,13 +78,13 @@ void OW10000_nibble::addNewLocationToStack(){
 	}
 
 	badge->setPixel((nibbleStack[nibbleStack_length-1] >> 4),(nibbleStack[nibbleStack_length-1] & 0b00001111),0);
-	
+
 	// Move all positions down, add to stack. Check for tail increase / decrease
 	for(int x = nibbleStack_length; x > 0; x--){
 		nibbleStack[x] = nibbleStack[x-1]; // Shift all of the nibble stack down to make room for the new one
 	}
 	nibbleStack[0] = snake_location;
-	
+
 	// Draw Changes on the playing field
 	if(nibbleStack_length >1){
 		// Set the previous head to full brightness
@@ -100,16 +100,16 @@ void OW10000_nibble::addNewLocationToStack(){
 // Play the game
 void OW10000_nibble::play() {
 	reset(); // Reset all the things
-	
+
 	// If both buttons are pressed, leave the game
 	while(stillPlaying) {
 		// Main game loop
-		
+
 		// Check if player wants to stop
 		if(badge->buttonAB_debounce(200)){
 			stillPlaying = false;
 		}
-		
+
 		// Check the controls, update direction as needed
 		if(badge->buttonL_debounce()){
 			if(direction !='r') direction = 'l';
@@ -120,11 +120,11 @@ void OW10000_nibble::play() {
 		} else if (badge->buttonD_debounce()){
 			if(direction !='u') direction = 'd';
 		}
-		
+
 		// Check for game update time, process commands
 		if(millis() > lastUpdate + snakeSpeed){
 			lastUpdate = millis(); // Update the game timer
-			
+
 			// Try to move the snake in the right direction
 			switch(direction){
 				case 'l':
@@ -135,7 +135,7 @@ void OW10000_nibble::play() {
 						// Where should this player be?
 						byte tempByte = ((snake_location >> 4)-1) << 4;
 						snake_location = (snake_location & 0b00001111) | tempByte;
-						
+
 						// Check collisions
 						if(snakeCollision(snake_location)){
 							gameOver(); // Snake has ran into itself. :(
@@ -143,7 +143,7 @@ void OW10000_nibble::play() {
 						addNewLocationToStack(); // Either empty, or has apple
 					}
 					break;
-					
+
 				case 'r':
 					if((snake_location & 0b11110000) == 0b11110000){
 						// Ran into a wall. Dead.
@@ -152,7 +152,7 @@ void OW10000_nibble::play() {
 						// Where should this player be?
 						byte tempByte = ((snake_location >> 4)+1) << 4;
 						snake_location = (snake_location & 0b00001111) | tempByte;
-						
+
 						// Check collisions
 						if(snakeCollision(snake_location)){
 							gameOver(); // Snake has ran into itself. :(
@@ -160,7 +160,7 @@ void OW10000_nibble::play() {
 						addNewLocationToStack(); // Either empty, or has apple
 					}
 					break;
-					
+
 				case 'u':
 					if((snake_location & 0b00001111) == 0b00000000){
 						// Ran into a wall. Dead.
@@ -168,7 +168,7 @@ void OW10000_nibble::play() {
 					} else {
 						// Where should this player be?
 						snake_location = snake_location - 1;
-						
+
 						// Check collisions
 						if(snakeCollision(snake_location)){
 							gameOver(); // Snake has ran into itself. :(
@@ -176,7 +176,7 @@ void OW10000_nibble::play() {
 						addNewLocationToStack(); // Either empty, or has apple
 					}
 					break;
-					
+
 				case 'd':
 					if((snake_location & 0b00001111) == 0b00001111){
 						// Ran into a wall. Dead.
@@ -184,7 +184,7 @@ void OW10000_nibble::play() {
 					} else {
 						// Where should this player be?
 						snake_location = snake_location + 1;
-						
+
 						// Check collisions
 						if(snakeCollision(snake_location)){
 							gameOver(); // Snake has ran into itself. :(
@@ -203,4 +203,3 @@ void OW10000_nibble::gameOver(){
 	// GAME OVER SHIT HERE!
 	stillPlaying = false;
 }
-
